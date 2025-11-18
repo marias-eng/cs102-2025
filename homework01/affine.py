bd5c398b525081f69d6be6ee6d9d46fab8058ea2
marias-eng <mariiasulima@gmail.com>
2025-11-18 11:14:35 +0300

    Реализована функция encrypt_affine

---
+++ homework01/affine.py
@@ -0,0 +1,20 @@
+def encrypt_affine(plaintext: str, a: int, b: int) -> str:
+    """
+    Encrypts plaintext using an Affine cipher.
+    """
+    ciphertext = ""
+    rus_alphabet_size = 32  # количество символов в русском алфавите без учета буквы ё
+    a_num = ord("а")
+    A_num = ord("А")
+    for char in plaintext:
+        if "А" <= char <= "Я":
+            size = A_num
+        elif "а" <= char <= "я":
+            size = a_num
+        else:
+            ciphertext += char
+            continue
+        x = ord(char) - size
+        encrypted_position = (a * x + b) % rus_alphabet_size
+        ciphertext += chr(encrypted_position + size)
+    return ciphertext
